ee54a6e59b4f5d8c931bc7e2e3c62e5c680dfb38
jeanetteobr <jeanette.obr@gmail.com>
2018-08-09 15:19:59 -0400

    Initial commit: first function created to show error message

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    var name = document.getElementById('name').value.trim()
+    if(name === ''){
+        showError()
+    }
+})
+
+// function showError() {
+//     var errorDiv = document.createElement('div')
+//     errorDiv.classList.add('error-msg')
+//     errorDiv.innerText = 'cannot be blank'
+
+//     var field = document.getElementById('input-field')
+//     errorDiv.classList.add('error-msg')
+//     errorDiv.innerText = 'cannot be blank'
+
+//     var field = document.getElementById('input-field')
+//     field.parentElement.appendChild(errorDiv)
+//     field.classList.add('error')
+// }
