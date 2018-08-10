1c7d953d8ffeaedcc186b87f287fe437044b12bc
jeanetteobr <jeanette.obr@gmail.com>
2018-08-10 11:49:02 -0400

    Step 1 accomplished. Yay.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // console.log('Add validation!');
 
-document.getElementById('parking-form').addEventListener('submit', function (event) {
+document.getElementById('parking-form').addEventListener('submit', function () {
     event.preventDefault()
-   
+
     var inputElements = document.getElementsByTagName('input')
-    for (var input of inputElements) {
-        console.log(input)
-        if(input.value.trim() === ('')) {
-            showError(event)  
-          } 
-}
+
+    for (var el of inputElements){
+        if(el.value.trim() === ''){
+            el.parentElement.classList.add('input-invalid')
+        }
+
+         else {
+            el.parentElement.classList.add('input-valid')
+        }
+    }
+
 })
 
-function showError() {
-      
-    var field = document.querySelector('.input-field')
-    field.classList.add('input-invalid')
-}
+// function showError() {
+//     var errorDiv = document.createElement('div')
+//     // field.classList.add('error-msg')
+
+//     var field = document.getElementById('name-field')
+//     field.classList.add('input-invalid')
+// }
